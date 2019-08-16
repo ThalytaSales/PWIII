f93891c6ae95823327fb5ebcf8d6cdf7718adaa6
Thalyta Sales <Thalyta.sales23@gmail.com>
2019-08-16 20:16:45 -0300

    Criação de clases

---
+++ app.ts
@@ -0,0 +1,15 @@
+class Aluno{
+    
+
+    constructor(public nome:string){}
+    exibir(){
+
+    console.log("Nome" + this.nome)
+    }
+}
+
+
+
+let ronqui = new Aluno("Rafael Ronqui")
+
+ronqui.exibir()
